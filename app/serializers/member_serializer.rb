73ff56efa607b237fa3ce40d71184590f49b93bc
thannemann <thannemann@gmail.com>
2026-05-06 09:07:06 -0400

    status
    add email and slack status to the member profile and member table

--- app/serializers/member_serializer.rb
+++ app/serializers/member_serializer.rb
@@ -11,8 +11,7 @@ class MemberSerializer < MemberSummarySerializer
              :member_contract_on_file,
              :group_name,
              :household_role,
-             :subscription_plan_id,
-             :mailtrap
+             :subscription_plan_id
 
   def card_id
     active_card = object.access_cards.to_a.find { |card| card.is_active? }
@@ -37,18 +36,6 @@ class MemberSerializer < MemberSummarySerializer
     invoice&.plan_id
   end
 
-  def mailtrap
-    event = object.mailtrap_event
-    return nil unless event
-
-    {
-      id: event.id,
-      timestamp: event.occurred_at&.in_time_zone("Eastern Time (US & Canada)")&.iso8601,
-      email: event.email,
-      status: event.status
-    }
-  end
-  
   def address
     {
       street: object.address_street,
--- app/serializers/member_summary_serializer.rb
+++ app/serializers/member_summary_serializer.rb
@@ -9,8 +9,32 @@ class MemberSummarySerializer < ActiveModel::Serializer
              :member_contract_signed_date,
              :member_contract_on_file,
              :notes,
+             :mailtrap,
+             :slack
 
   def member_contract_on_file
     !object.member_contract_signed_date.nil?
+  end
+
+  def mailtrap
+    event = object.mailtrap_event
+    return nil unless event
+
+    {
+      id: event.id,
+      timestamp: event.occurred_at&.in_time_zone("Eastern Time (US & Canada)")&.iso8601,
+      email: event.email,
+      status: event.status
+    }
+  end
+
+  def slack
+    slack_user = SlackUser.find_by(member_id: object.id)
+    return nil unless slack_user
+
+    {
+      slack_id: slack_user.slack_id,
+      name: slack_user.real_name.presence || slack_user.name
+    }
   end
 end
