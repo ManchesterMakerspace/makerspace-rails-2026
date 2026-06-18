48fb6aa94c047193618bf1a9756a39779f871327
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-06-18 11:49:53 -0400

    Prevent service invites on email update

--- app/models/member.rb
+++ app/models/member.rb
@@ -394,8 +394,6 @@ class Member
   end
 
   def publish_update
-    # Invite to Slack, Google if email changed.
-    publish(:email_changed) if previous_changes.key?("email")
     publish(:billing_info_changed) if previous_changes.keys.any? { |attr| [:firstname, :lastname].include?(attr.to_sym) }
   end
 
