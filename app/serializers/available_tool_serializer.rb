97f6a9cdce38158c5f6a31dc115dedfebe6f14a0
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-06-30 01:00:43 -0400

    Fix available tool prerequisite serialization

--- app/serializers/available_tool_serializer.rb
+++ app/serializers/available_tool_serializer.rb
@@ -1,5 +1,15 @@
-class AvailableToolSerializer < ToolSerializer
-  attributes :unmet_prerequisite_ids, :unmet_prerequisite_names, :requestable
+class AvailableToolSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :disabled, :announce, :announce_channel,
+             :shop_id, :shop_name, :prerequisite_ids, :prerequisite_names,
+             :unmet_prerequisite_ids, :unmet_prerequisite_names, :requestable
+
+  attribute :shop_name do
+    object.shop.try(:name)
+  end
+
+  attribute :prerequisite_names do
+    object.prerequisites.map(&:name)
+  end
 
   def unmet_prerequisite_ids
     object.respond_to?(:unmet_prerequisite_ids) ? object.unmet_prerequisite_ids : []
