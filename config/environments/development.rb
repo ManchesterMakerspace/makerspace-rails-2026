c025d102540f849b6c17f3350b637e1435cf39db
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-07-04 13:07:23 -0400

    Enable public file server based on environment variable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Serving static files from the `/public` folder by default unless Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   if config.public_file_server.enabled?
     $stderr.puts '[config] RAILS_SERVE_STATIC_FILES=true'
   else
