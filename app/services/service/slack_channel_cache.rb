4bffdb1424a8a6fe4c1f9771d09552576f23e13a
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-09-05 10:52:13 -0400

    Improve normalization of channel names
    Refactor channel name normalization to handle '#' prefix.

--- app/services/service/slack_channel_cache.rb
+++ app/services/service/slack_channel_cache.rb
@@ -47,7 +47,6 @@ module Service
         # New approach takes bare channel name strings and prepends '#' only if not already there:
         normalized = raw_value.sub(/\A#+/, "").downcase
         return value if normalized.blank?
-        end
         return "#" + normalized
       end
 
