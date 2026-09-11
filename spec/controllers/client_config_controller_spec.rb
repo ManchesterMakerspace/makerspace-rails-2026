142308ad6961fc5b8c5c7ec9bc81e7eb4b80d321
Kevin <kkadow@manchestermakerspace.org>
2026-09-10 21:46:27 -0400

    QR as PNG

--- spec/controllers/client_config_controller_spec.rb
+++ spec/controllers/client_config_controller_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe ClientConfigController, type: :controller do
+  it "publishes APP_DOMAIN for public tool QR destinations" do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with("APP_DOMAIN").and_return(" portal.example.test ")
+    get :index, format: :json
+    expect(JSON.parse(response.body)["app_domain"]).to eq("portal.example.test")
+  end
+
   it "publishes the normalized WIKI_URL for runtime clients" do
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with("WIKI_URL")
